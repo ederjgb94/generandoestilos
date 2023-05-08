3a9e6b6e148f840548eb0723c2d77b1c424bea7d
ederjgb94 <a2133330155@alumnos.uat.edu.mx>
2023-05-08 17:16:49 -0600

    ok

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,18 @@ import 'package:share_whatsapp/share_whatsapp.dart';
 import 'package:flimer/flimer.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-const _kTextMessage = 'Share text from share_whatsapp flutter package '
-    'https://pub.dev/packages/whatsapp_share';
+const _kTextMessage =
+    'Hola *ROBERTO!* Te recordamos que tienes una visita programada en nuestro taller _AUTROMOTRIZ MARTINEZ_ *MAÑANA* a las *11:30am* para *SERVICIO DE FRENOS*. ¡Esperamos verte pronto!';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MaterialApp(
+      home: MyApp(),
+    ),
+  );
+  // runApp(const MaterialApp(
+  //   home: HomePage(),
+  // ));
 }
 
 class MyApp extends StatefulWidget {
@@ -77,68 +84,98 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Share WhatsApp Example'),
-        ),
-        body: ListView(
-          children: ListTile.divideTiles(
-            context: context,
-            tiles: [
-              const ListTile(title: Text('STATUS INSTALLATION')),
-              ...WhatsApp.values.map((type) {
-                final status = _mapInstalled[type];
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Share WhatsApp Example'),
+      ),
+      body: ListView(
+        children: ListTile.divideTiles(
+          context: context,
+          tiles: [
+            // const ListTile(title: Text('STATUS INSTALLATION')),
+            // ...WhatsApp.values.map((type) {
+            //   final status = _mapInstalled[type];
 
-                return ListTile(
-                  title: Text(type.toString()),
-                  trailing: status != null
-                      ? Text(status)
-                      : const CircularProgressIndicator.adaptive(),
+            //   return ListTile(
+            //     title: Text(type.toString()),
+            //     trailing: status != null
+            //         ? Text(status)
+            //         : const CircularProgressIndicator.adaptive(),
+            //   );
+            // }),
+            // const ListTile(title: Text('SHARE CONTENT')),
+            // ListTile(
+            //   title: const Text('Share Text'),
+            //   trailing: const Icon(Icons.share),
+            //   onTap: () => shareWhatsapp.shareText(_kTextMessage),
+            // ),
+            // ListTile(
+            //   title: const Text('Share Image'),
+            //   trailing: const Icon(Icons.share),
+            //   onTap: () async {
+            //     final file = await flimer.pickImage();
+            //     if (file != null) {
+            //       shareWhatsapp.shareFile(file);
+            //     }
+            //   },
+            // ),
+            ListTile(
+              title: const Text('Enviar PDF a número especifico'),
+              trailing: const Icon(Icons.share),
+              onTap: () async {
+                final dir = (await getDirectory()).path;
+                XFile file = XFile('$dir/example6.pdf', name: 'example.pdf');
+
+                await shareWhatsapp.share(
+                  text: 'Hello World',
+                  file: file,
+                  phone: '+52 1 833 295 6072',
                 );
-              }),
-              const ListTile(title: Text('SHARE CONTENT')),
-              ListTile(
-                title: const Text('Share Text'),
-                trailing: const Icon(Icons.share),
-                onTap: () => shareWhatsapp.shareText(_kTextMessage),
+              },
+            ),
+            ListTile(
+              title: const Text('Enviar Mensaje de Cita'),
+              trailing: const Icon(Icons.share),
+              onTap: () => shareWhatsapp.share(
+                text: _kTextMessage,
+                // change with real whatsapp number
+                phone: '+52 1 833 295 6072',
               ),
-              ListTile(
-                title: const Text('Share Image'),
-                trailing: const Icon(Icons.share),
-                onTap: () async {
-                  final file = await flimer.pickImage();
-                  if (file != null) {
-                    shareWhatsapp.shareFile(file);
-                  }
-                },
-              ),
-              ListTile(
-                title: const Text('Share Text & Image'),
-                trailing: const Icon(Icons.share),
-                onTap: () async {
-                  final dir = (await getDirectory()).path;
-                  XFile file = XFile('$dir/example5.pdf', name: 'example.pdf');
-
-                  await shareWhatsapp.share(
-                    text: 'Hello World',
-                    file: file,
-                    phone: '+52 1 833 327 6522',
-                  );
-                },
-              ),
-              ListTile(
-                title: const Text('Share Text on Specific Phone Number'),
-                trailing: const Icon(Icons.share),
-                onTap: () => shareWhatsapp.share(
-                  text: _kTextMessage,
-                  // change with real whatsapp number
-                  phone: '+52 1 833 382 0929',
-                ),
-              ),
-            ],
-          ).toList(),
-        ),
+            ),
+            ListTile(
+              title: const Text('Generar Reporte'),
+              trailing: const Icon(Icons.share),
+              onTap: () async {
+                final dir = (await getDirectory()).path;
+                var pdf = await generateReporte(PdfPageFormat.letter);
+                final file = File('$dir/example6.pdf');
+                await file.writeAsBytes(pdf);
+              },
+            ),
+            ListTile(
+              title: const Text('Ver PDF Generado'),
+              trailing: const Icon(Icons.share),
+              onTap: () async {
+                final dir = (await getDirectory()).path;
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Scaffold(
+                      appBar: AppBar(),
+                      body: PDFView(
+                        filePath: '$dir/example6.pdf',
+                        enableSwipe: true,
+                        swipeHorizontal: true,
+                        autoSpacing: false,
+                        pageFling: false,
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ],
+        ).toList(),
       ),
     );
   }
